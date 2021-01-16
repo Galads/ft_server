d6e7449c417d112ed4f5b1994e5925960ecaff99
Belwas Rice <brice@mi-p5.msk.21-school.ru>
2021-01-16 09:05:01 +0300

    create dockerFile

---
+++ src/script_install.sh
@@ -0,0 +1,9 @@
+apt-get -y update
+apt install -y ufw
+ufw enable
+apt-get -y install nginx
+ufw app list
+ufw allow 'Nginx HTTP'
+#ufw status
+#systemctl status nginx
+ip addr show eth0 | grep inet | awk '{ print $2; }' | sed 's/\/.*$//' #172.17.0.2
